a984f0271b5be51045accaa165f3f927335ae52a
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-08-15 14:38:52 +0100

    feat: Add NeoVim hook

---
+++ hooks/neovim.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufWritePost" }, {
+    callback = function(event)
+        local file = event.file
+
+        if file:sub(1, 6) == "oil://" then
+            file = file:sub(7)
+        end
+
+        vim.system({ "memy", "note", file }, { detach = true }, function()
+            vim.schedule(function()
+                if out.code ~= 0 then
+                    vim.notify(
+                        "From external command: " .. out.stderr,
+                        vim.log.levels.ERROR
+                    )
+                end
+            end)
+        end)
+    end,
+})
