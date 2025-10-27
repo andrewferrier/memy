8d704705b26d0c5579d76c77bb6331a2496824dc
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2025-10-27 20:24:22 +0000

    refactor: Simplify detect_shell

--- src/utils.rs
+++ src/utils.rs
@@ -33,21 +33,18 @@ pub fn timestamp_age_hours(now: UnixTimestamp, timestamp: UnixTimestamp) -> Unix
 }
 
 pub fn detect_shell() -> Option<clap_complete::Shell> {
-    std::env::var("SHELL").ok().as_deref().and_then(|path| {
-        let name = Path::new(path)
-            .file_name()
-            .and_then(|os_str| os_str.to_str())
-            .map(str::to_lowercase)?;
+    let name = std::env::var("SHELL")
+        .ok()
+        .and_then(|path| path.rsplit('/').next().map(str::to_lowercase))?;
 
-        match name.as_str() {
-            "bash" => Some(clap_complete::Shell::Bash),
-            "zsh" => Some(clap_complete::Shell::Zsh),
-            "fish" => Some(clap_complete::Shell::Fish),
-            "powershell" => Some(clap_complete::Shell::PowerShell),
-            "elvish" => Some(clap_complete::Shell::Elvish),
-            _ => None,
-        }
-    })
+    match name.as_str() {
+        "bash" => Some(clap_complete::Shell::Bash),
+        "zsh" => Some(clap_complete::Shell::Zsh),
+        "fish" => Some(clap_complete::Shell::Fish),
+        "powershell" => Some(clap_complete::Shell::PowerShell),
+        "elvish" => Some(clap_complete::Shell::Elvish),
+        _ => None,
+    }
 }
 
 pub fn expand_tilde<P: AsRef<Path> + ?Sized>(path: &'_ P) -> Cow<'_, Path> {
